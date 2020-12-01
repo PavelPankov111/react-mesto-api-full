dd3e0c54be043baa8837527aaf1e95d0f19baf40
«Pavel <Mr.Sawv@gmail.com>
2020-12-01 18:03:57 +0300

    v9

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -29,7 +29,7 @@ module.exports.updateAvatar = (req, res, next) => {
     new: true,
     runValidators: true,
   })
-    .then((user) => res.send({ data: user }))
+    .then((user) => res.send(user))
     .catch(next);
 };
 
