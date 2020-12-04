c4eecd41a1ec112886b09d0a3ad8732300d7dffe
«Pavel <Mr.Sawv@gmail.com>
2020-12-05 02:39:11 +0300

    etreegdhfdre

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -30,7 +30,7 @@ router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     about: Joi.string().required().min(2).max(30),
-  }).unknown(true),
+  }),
 }), updateUser);
 
 router.patch('/users/me/avatar', celebrate({
